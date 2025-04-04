5294cb77716ff0af7c718ed574904aef24342dbb
James Ruscingno <Jimtitans21@aol.com>
2025-04-04 03:14:43 -0400

    added form capabilities and transition

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostNameInput = newPostModal.querySelector("#card-image-input");
+const newPostLinkInput = newPostModal.querySelector("#profile-description-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +33,21 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit (evt) {
+  evt.preventDefault ();
+ profileNameEl.textContent = editProfileNameInput.value;
+ profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+ editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleNewPostSubmit (evt) {
+  evt.preventDefault ();
+  console.log("newPostName", newPostNameInput.value);
+  console.log("newPostLinkInput", newPostLinkInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
